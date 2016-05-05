a086af0e49df065f478a8f08d824b2bd9e64b812
Jeffrey Hackford and Kevin Deganos <kevin@>
2016-05-05 08:53:23 -0700

    add unit specs for review class

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -26,10 +26,43 @@ public class Review {
     return restaurant_id;
   }
 
+  public int getId() {
+    return id;
+  }
+
   public static List<Review> all() {
     String sql = "SELECT id, review, rating, restaurant_id FROM reviews";
     try (Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).executeAndFetch(Review.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object review) {
+    if (!(review instanceof Review)) {
+      return false;
+    } else {
+      Review newReview = (Review) review;
+      return this.getReview().equals(newReview.getReview());
+    }
+  }
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO reviews(review, rating, restaurant_id) VALUES (:review, :rating, :restaurant_id)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("review", this.review)
+        .addParameter("rating", this.rating)
+        .addParameter("restaurant_id", this.restaurant_id)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+  public static Review find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM reviews WHERE id=:id";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Review.class);
     }
   }
 }
--- src/test/java/ReviewTest.java
+++ src/test/java/ReviewTest.java
@@ -21,4 +21,34 @@ public class ReviewTest {
     Review newReview = new Review("Review 1", 4, 1);
     assertEquals(true, newReview instanceof Review);
   }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Review.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfBothNamesAreTheSame_True() {
+    Review firstReview = new Review("Review 1", 4, 1);
+    Review secondReview = new Review("Review 1", 4, 1);
+    assertTrue(firstReview.equals(secondReview));
+  }
+
+  @Test
+  public void save_savesInstanceToDBWithId_True() {
+    Review newReview = new Review("Review 1", 4, 1);
+
+    newReview.save();
+
+    Review savedReview = Review.all().get(0);
+    assertEquals(newReview.getId(), savedReview.getId());
+  }
+
+  @Test
+  public void find_returnsCorrectInstanceOfReview_True() {
+    Review newReview = new Review("Review 1", 5, 1);
+    newReview.save();
+    Review foundReview = Review.find(newReview.getId());
+    assertTrue(newReview.equals(foundReview));
+  }
 }
